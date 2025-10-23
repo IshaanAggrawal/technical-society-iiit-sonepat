e2074c8706266610611f534fe1466f86fe362f5b
Shubham Singh <shubhsoch@gmail.com>
2025-10-23 17:19:09 +0530

    metafox-game-modules

---
+++ frontend/src/app/blockchain/metafox/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import MetafoxGame from './components/MetafoxGame';
+
+export default function MetafoxPage() {
+  return (
+    <div className="w-full h-screen overflow-hidden relative">
+      <MetafoxGame />
+    </div>
+  );
+}
